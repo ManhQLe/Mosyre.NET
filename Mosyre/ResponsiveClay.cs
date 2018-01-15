9d9f0913514b067642063a5c8c828a77a7602b4d
Manh Le <quang.manh@gmail.com>
2018-01-14 21:35:49 -0600

    Finished ResponsiveClay

--- Mosyre/ResponsiveClay.cs
+++ Mosyre/ResponsiveClay.cs
@@ -1,28 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Mosyre
 {
-	public delegate void ResponseFunc(ResponsiveClay c);
-	
+	public delegate void ResponseFunc(ResponsiveClay c,object connectPoint = null);
+	public delegate void InitFunc(ResponsiveClay c);
 
 	public class ResponsiveClay: AttribClay
 	{
 		Dictionary<object, IClay> _contacts;
 		Dictionary<object, object> _signalStore;
+		List<object> _collectedPoints;
 		public ResponsiveClay(Dictionary<string,object> agr) : base(agr) {
 			_contacts = new Dictionary<object, IClay>();
 			_signalStore = new Dictionary<object, object>();
+			_collectedPoints = new List<object>();
 
+		}
 
+		public T GetInput<T>(object connectPoint) {
+			object o = _signalStore[connectPoint];
+			return o == null ? default(T) : (T)o;
+		}
+
+		public InitFunc Init {
+			get { return GetAgrementProp<InitFunc>("Init", defaultInit); }
+			set {
+				_agreement["Init"] = value;
+			}
 		}
 
 		public ResponseFunc Response {
 			get {
-				return GetAgrementProp<ResponseFunc>("Response", defaultFunction);
+				return GetAgrementProp<ResponseFunc>("Response", defaultResponse);
 			}
 			set {
 				_agreement["Response"] = value;
@@ -43,8 +57,10 @@ namespace Mosyre
 				return _signalStore[connectPoint];
 			}
 			set {
-				//This is important to check for fire stage
-				//Should be thread
+						
+
+				signalProcessing(connectPoint, value);
+				
 			}
 		}
 
@@ -58,9 +74,11 @@ namespace Mosyre
 		}
 
 		public override void onCommunication(IClay fromClay, object atConnectionPoint, object signal)
-		{			
+		{
 			//Check to see if it is in connectiton list
-
+			if (_contacts[atConnectionPoint] == fromClay) {
+				this[atConnectionPoint] = signal;
+			}
 		}
 
 		public override void onConnection(IClay withClay, object atConnectionPoint)
@@ -68,9 +86,40 @@ namespace Mosyre
 			_contacts[atConnectionPoint] = withClay;
 		}
 
-		static private void defaultFunction(ResponsiveClay c)
+		private void signalProcessing(object connectPoint, object signal) {
+			List<Object> cps = ConnectPoints;
+			//This is important to check for fire stage		
+			bool isInput = cps.IndexOf(connectPoint) >= 0;
+			if (isInput)
+			{				
+				_signalStore[connectPoint] = signal;
+				//Check if collected point ?
+				bool hasCollected = _collectedPoints.IndexOf(connectPoint) >= 0;
+				if (!hasCollected)
+					_collectedPoints.Add(connectPoint);
+
+				if (_collectedPoints.Count == cps.Count)
+				{
+					Response(this, connectPoint);
+
+				}
+
+			}
+			else // Output
+			{
+				IClay o = _contacts[connectPoint];
+				o?.onCommunication(this, connectPoint, signal);
+			}
+		}
+
+		static private void defaultResponse(ResponsiveClay c, object connectPoint)
+		{
+			
+		}
+		static private void defaultInit(ResponsiveClay c)
 		{
 
 		}
+
 	}
 }
