3982349695f5cd309c247eb6d24a4ed44b96e1ee
Manh Le <quang.manh@gmail.com>
2018-01-17 23:18:07 -0600

    Add onConnection for Conduit

---
+++ Mosyre/Conduit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosyre
+{
+	public class Conduit : AttribClay
+	{
+		List<KeyValuePair<object, IClay>> _contacts;
+
+		public Conduit():this(new Dictionary<string, object>()) {
+		}
+
+		public Conduit(Dictionary<string, object> agreement) : base(agreement)
+		{
+			_contacts = new List<KeyValuePair<object, IClay>>();
+		}
+
+		public object Signal {
+			set { }
+		}
+
+		public override void onCommunication(IClay fromClay, object atConnectionPoint, object signal)
+		{
+			
+		}
+
+		public override void onConnection(IClay withClay, object atConnectionPoint)
+		{
+			int dix = _contacts.fin
+		}
+	}
+}
